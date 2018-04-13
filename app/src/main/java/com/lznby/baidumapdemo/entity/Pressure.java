c80f899af10a97c183c9e719bbc3de2b1d8c64a3
lznby <lznbys@gmail.com>
2018-04-13 08:23:58 +0800

    绘制水压曲线完成。

---
+++ app/src/main/java/com/lznby/baidumapdemo/entity/Pressure.java
@@ -0,0 +1,11 @@
+package com.lznby.baidumapdemo.entity;
+
+/**
+ * @author Admin
+ * @version $Rev$
+ * @des ${TODO}
+ * @updateAuthor $Author$
+ * @updateDes ${TODO}
+ */
+public class Pressure {
+}
---
+++ app/src/main/java/com/lznby/baidumapdemo/util/DrawChart.java
@@ -0,0 +1,11 @@
+package com.lznby.baidumapdemo.util;
+
+/**
+ * @author Admin
+ * @version $Rev$
+ * @des ${TODO}
+ * @updateAuthor $Author$
+ * @updateDes ${TODO}
+ */
+public class DrawChart {
+}
